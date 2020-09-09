8da48bf4d378d66ae5e289f2fc81454507f57d8e
Gabriel-J3sus <gabr.jesus001@gmail.com>
2020-09-09 11:37:09 -0300

    3º commit

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
-import { Container, Content } from './styles';
+import {
+  Container,
+  HeaderWrapper,
+  Header,
+  DropboxLogo,
+  Content
+} from './styles';
 
 interface Props {
     variant: 'blue' | 'beige' | 'white' | 'black';
@@ -9,12 +15,28 @@ interface Props {
 }
 
 const Section: React.FC<Props> = ({ variant, title, description }) => {
+  const buttonVariant = Math.round(Math.random());  // 0 or 1
+
+  function handleToggle() {
+    if (window.toggleActiveMenu) window.toggleActiveMenu();
+  }
+
   return (
       <Container className={variant}>
+        <HeaderWrapper>
+          <Header>
+            <h1>
+              <DropboxLogo />
+              <span> Dropbox </span>
+            </h1>
+
+            <button onClick={handleToggle}> {buttonVariant === 0 ? 'Acessar' : 'Interagir'} </button>
+          </Header>
+        </HeaderWrapper>
+
         <Content>
             <h2> {title} </h2>
             <p> {description} </p>
-
         </Content>
       </Container>
   );
